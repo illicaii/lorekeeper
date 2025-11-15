b8ae9809bd74c2dfd80b34b9012459bbf0c5e9c9
Kbellott <vilagyent@gmail.com>
2025-11-15 11:38:14 -0800

    libSkills: Initial Commit

---
+++ config/lorekeeper/ext-tracker/libSkills.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'key'      => 'Skills_library',
+    'wiki_key' => 'libSkills',
+    'creators' => json_encode([
+        'Vilagyent'     => 'https://github.com/Kbellott/lorekeeper',
+    ]),
+    'version' => '1.0.0',
+];
