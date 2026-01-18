a545f7f41fafd3d0b052b0b88f3c87dc11fc8caf
Kbellott <Kbellott@users.noreply.github.com>
2026-01-18 06:43:32 +0000

    refactor: fix blade formatting

--- resources/views/admin/skills/_remove_character_skills.blade.php
+++ resources/views/admin/skills/_remove_character_skills.blade.php
@@ -1,8 +1,8 @@
-@if($image)
-    {!! Form::open(['url' => 'admin/character/image/'.$image->id.'/skills/remove']) !!}
+@if ($image)
+    {!! Form::open(['url' => 'admin/character/image/' . $image->id . '/skills/remove']) !!}
 
     <p>You are about to <strong>remove all skills</strong> from this character.
-            This is not reversible. <br> Are you sure you want to remove skills?</p>
+        This is not reversible. <br> Are you sure you want to remove skills?</p>
 
     <div class="text-center">
         {!! Form::submit('Remove Character Skills', ['class' => 'btn btn-danger']) !!}
